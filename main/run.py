41ae1594416bab4f60f5f551ce5072091e007b4c
Ayush Abrol <ayushabrol13@gmail.com>
2022-06-10 15:11:56 +0530

    update rum.py

--- main/run.py
+++ main/run.py
@@ -4,7 +4,7 @@ import main
 # Greeting the user
 main.greet()
 # Engine introduction to the user as "Jarvis"...Taken from the entertainment industry...
-main.talk("Jarvis here..")
+main.talk("Your AI here..")
 main.talk("Nice to meet you.")
 main.talk('How can I help you?')
 
